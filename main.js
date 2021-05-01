7f2b48d6674fa4737b92ced4f091740c84b98dac
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-05-01 16:03:10 +0900

    add:task

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var app = new Vue({
   el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
   methods: {
     addItem: function(event) {
-      alert();
+      var todo = {
+        item: this.newItem
+      };
+      this.todos.push(todo);
     }
   }
 })
