472a3b9f2d9e3cde205fd0486e9c06b073c159dd
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-05-01 22:29:30 +0900

    add:checkbox

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ var app = new Vue({
   methods: {
     addItem: function(event) {
       if(this.newItem == '') return;
-      
+
       var todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       };
       this.todos.push(todo);
       this.newItem = '';
